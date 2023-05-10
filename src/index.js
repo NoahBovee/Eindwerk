85f14dc68aa487a4e40b01d1007463dfc21f3d12
Noah Bovee <noahbov@gmail.com>
2023-05-10 12:30:06 +0200

    player-movement

--- src/index.js
+++ src/index.js
@@ -5,20 +5,45 @@ const config = {
   height: 800,
   width: 800,
   backgroundColor: "#2d6b2d",
+  physics: {
+    default: "arcade",
+  },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
-new Phaser.Game(config);
+let player = null;
+let cursors = null;
+
+const playerSpeed = 1;
 
 function preload() {
   this.load.image("player", "../assets/bird.png");
 }
 
 function create() {
-  this.add
+  player = this.physics.add
     .sprite(config.height / 2, config.height / 2, "player")
-    .setOrigin(0, 0);
+    .setOrigin(0.5);
+
+  cursors = this.input.keyboard.addKeys("Z,Q,S,D");
 }
+
+function update() {
+  if (cursors.Z.isDown) {
+    player.y -= playerSpeed;
+  } else if (cursors.S.isDown) {
+    player.y += playerSpeed;
+  }
+
+  if (cursors.Q.isDown) {
+    player.x -= playerSpeed;
+  } else if (cursors.D.isDown) {
+    player.x += playerSpeed;
+  }
+}
+
+new Phaser.Game(config);
