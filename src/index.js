1c1d270f863f205e02c5652a7d1d53914a01414f
Noah Bovee <noahbov@gmail.com>
2023-05-10 11:44:21 +0200

    load sprite

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ const config = {
   height: 800,
   width: 800,
   backgroundColor: "#2d6b2d",
+  scene: {
+    preload,
+    create,
+  },
 };
 
 new Phaser.Game(config);
+
+function preload() {
+  this.load.image("player", "../assets/bird.png");
+}
+
+function create() {
+  this.add
+    .sprite(config.height / 2, config.height / 2, "player")
+    .setOrigin(0, 0);
+}
